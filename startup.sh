9f52ddde2863a2cfa89114130d227726a5b0bbd6
karneeshkar <karneeshkar68@gmail.com>
2025-02-23 20:10:46 +0530

    yes

--- startup.sh
+++ startup.sh
@@ -4,11 +4,9 @@ set -e
 pip install -r requirements.txt
 
 # Download the NLTK vader lexicon (if not already downloaded)
-python -c "import nltk; nltk.download(\"vader_lexicon\")"
 
 # Start the Celery worker in the background
 celery -A server.celery worker -l info &
 
 # Start the Uvicorn server
-echo "this crossed here bitchesss"
 uvicorn server:app --host 0.0.0.0 --port 8000
